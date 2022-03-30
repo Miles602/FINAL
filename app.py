b589bbe0fab5ad22590ebb6185b0e4f1fb7077a3
Miles602 <milesmd1426@gmail.com>
2022-03-29 23:37:31 -0400

    4

---
+++ app.py
@@ -0,0 +1,48 @@
+import errno
+from gzip import READ
+from os import abort
+from flask import Flask, redirect, render_template, request, url_for
+
+app = Flask(__name__)
+
+@app.get('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login', methods=["GET","POST"])
+def login():
+    error=None
+    if request.method=='POST':
+        if request.form['username'] != 'admin' or request.form["password"] != "admin":
+            error="invalid resonse, please try again"
+        else:
+            return redirect('/')
+
+    return render_template("login.html",error=error)
+
+
+@app.get('/register/new')
+
+def register_form():
+    return render_template("registration.html")
+
+
+@app.post('/register')
+def register():
+    error=None
+    user_name=request.form.get('username','')
+    password=request.form.get("password",'')
+    if user_name == '' or password=='':
+            error="You have not filled in your information"
+    else:
+        return redirect("/")
+    return render_template("registration.html",error=error)
+    
+
+
+
+
+
+if __name__ == '__main__':
+    app.run()
